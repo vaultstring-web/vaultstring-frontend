a84cacc592e16937e16c748e449463a543004d99
Uchindami Gondwe <143083979+guchindami@users.noreply.github.com>
2026-01-07 20:59:17 +0200

    Updated settlements and translation names and details

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,31 +1,6 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/6e307_next_dist_compiled_135f06ca._.js",
-      "static/chunks/6e307_next_dist_shared_lib_b6c92be8._.js",
-      "static/chunks/6e307_next_dist_client_35a893cd._.js",
-      "static/chunks/6e307_next_dist_7c2b871c._.js",
-      "static/chunks/6e307_next_app_4bdf5e23.js",
-      "static/chunks/[next]_entry_page-loader_ts_38e7bd5b._.js",
-      "static/chunks/8becc_react-dom_0ee3df1c._.js",
-      "static/chunks/5448c__pnpm_b99e3f8b._.js",
-      "static/chunks/[root-of-the-server]__54e10a5d._.js",
-      "static/chunks/Desktop_VaultString_Projects_vaultstring-frontend_pages__app_2da965e7._.js",
-      "static/chunks/turbopack-Desktop_VaultString_Projects_vaultstring-frontend_pages__app_7d25205c._.js"
-    ],
-    "/_error": [
-      "static/chunks/6e307_next_dist_compiled_135f06ca._.js",
-      "static/chunks/6e307_next_dist_shared_lib_bedc0ed2._.js",
-      "static/chunks/6e307_next_dist_client_35a893cd._.js",
-      "static/chunks/6e307_next_dist_14b22b50._.js",
-      "static/chunks/6e307_next_error_1712bf67.js",
-      "static/chunks/[next]_entry_page-loader_ts_11aabbd4._.js",
-      "static/chunks/8becc_react-dom_0ee3df1c._.js",
-      "static/chunks/5448c__pnpm_b99e3f8b._.js",
-      "static/chunks/[root-of-the-server]__b52226c4._.js",
-      "static/chunks/Desktop_VaultString_Projects_vaultstring-frontend_pages__error_2da965e7._.js",
-      "static/chunks/88e7d_VaultString_Projects_vaultstring-frontend_pages__error_43484dd6._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "polyfillFiles": [
